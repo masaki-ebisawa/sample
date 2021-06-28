fef7efabc9bb770b5e9d243e02692685efa14a34
masaki <homu51129@gmail.com>
2021-06-28 04:25:40 +0000

    ruby 07

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,16 +4,22 @@ class FavoritesController < ApplicationController
   end
   
   def create
-    favorite = Favorite.new
-    favorite.user_id = current_user.id
+    favorite = Favorite.new #Favoriteクラスのインスタンス作成
+    favorite.user_id = current_user.id #current_userのidを変数に代入
     favorite.topic_id= params[:topic_id]
     
-    if favorite.save
+    if favorite.save #favoriteが保存できているかどうかで条件分岐
       redirect_to topics_path, success: 'お気に入りに登録しました'
       
     else
       redirect_to topics_path, danger: 'お気に入りの登録に失敗しました'
       
     end
+  end
+  
+  def destroy
+    @favorite = favorite.find_by(user_id: current_user.id, topic_id: params[:topic_id])
+    @favorite.destroy
+    redirect_to topic_path, success: 'お気に入りを取り消しました'
   end
 end
