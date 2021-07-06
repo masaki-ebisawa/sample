f42c4358706fee4d318dbd1e276d03a8c71f0c95
masaki <homu51129@gmail.com>
2021-07-06 13:20:55 +0000

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    if session[:user_id]
+     @current_user ||= User.find_by(id: session[:user_id]) #@current_user = @current_user || User.find_by(id: session[:user_id])と同じ意味
+    end
   end
 
   def logged_in?
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
 
   has_many :topics
   has_many :favorites
-  has_many :favorite_topics, through: favorites, source: 'topic'
+  has_many :favorite_topics, through :favorites, source: 'topic'
   
   mount_uploader :image, ImageUploader
 
