132bb3f27e06260822ab00a92f5d325888d585c5
masaki <homu51129@gmail.com>
2021-07-06 12:53:46 +0000

    heroku

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,9 +1,10 @@
 class Topic < ApplicationRecord
   validates :user_id, presence: true
   validates :description, presence: true
-  vakidates :image, presence: true
+  validates :image, presence: true
   
   belongs_to :user
+  has_many :favorites
   
   mount_uploader :image, ImageUploader
 end
