1c98807b93f5f8b579736a534af8906cc699454e
masaki <homu51129@gmail.com>
2021-07-08 11:12:19 +0000

    heroku

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,7 @@ Rails.application.routes.draw do
   get 'sessions/new'
   get 'users/new'
   get 'pages/link'
-  root 'pages#link'
   get 'pages/help'
-  root 'pages#index'
   get 'pages/index'
   root 'pages#index'
   resources :users
